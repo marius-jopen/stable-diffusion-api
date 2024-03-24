7d69b9de991e2f395bab8b4d4aad1934fabdc85a
marius-jopen <marius.jopen@thepeople.world>
2024-03-24 19:00:17 +0100

    basic connection to deforum done

--- server/components/VideoGenerator.js
+++ server/components/VideoGenerator.js
@@ -1,6 +1,4 @@
 import fetch from 'node-fetch';
-import fs from 'fs';
-import path from 'path';
 
 class VideoGenerator {
   constructor(outputDir) {
@@ -9,7 +7,7 @@ class VideoGenerator {
 
   async generateVideo(parameters) {
     try {
-      const response = await fetch("http://127.0.0.1:7860/sdapi/v1/txt2img", {
+      const response = await fetch("http://127.0.0.1:7860/deforum_api/batches", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -18,19 +16,12 @@ class VideoGenerator {
       });
 
       const jsonResponse = await response.json();
-      const base64ImageData = jsonResponse.images[0];
-      const imageData = Buffer.from(base64ImageData, 'base64');
-      const timestamp = Date.now();
-      const imageName = `image_${timestamp}.png`;
-      const outputPath = path.join(this.outputDir, imageName);
-
-      fs.writeFileSync(outputPath, imageData);
-      console.log(`Image saved at: ${outputPath}`);
-      const imageUrl = `/output/txt2vid/${imageName}`;
+      console.log(jsonResponse); // Log the entire response
       
+      // If you want to return some info or data to the caller
       return {
-        imageUrl,
-        info: "Image generated and saved successfully!"
+        message: "Check the console for the logged response.",
+        info: "Operation completed successfully!"
       };
     } catch (error) {
       console.error('Error in VideoGenerator:', error);
